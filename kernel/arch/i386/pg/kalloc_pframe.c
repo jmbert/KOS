689defe5faeb42d581268ed04aedc8015d6e3b2a
jmbert <joe2157b@gmail.com>
2023-07-05 16:13:51 +0100

    Fixed kalloc_pframe

--- kernel/arch/i386/pg/kalloc_pframe.c
+++ kernel/arch/i386/pg/kalloc_pframe.c
@@ -8,5 +8,6 @@ uint8_t framemap[8192];
 pframe_t kalloc_pframe(void) {
     size_t i = 3;
     for (;framemap[i] == PAGE_USED;i++);
+    framemap[i] = PAGE_USED;
     return (0x200000 + (i * PG_SIZE_SMALL));
 }
