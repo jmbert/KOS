66c8f23fc45a10cf33a4c0dc933ebb126c0d73eb
jmbert <joe2157b@gmail.com>
2023-07-05 16:12:08 +0100

    Fixed malloc

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -7,11 +7,24 @@
 extern "C" {
 #endif
 
-#if defined __is_kernel
+#if defined __is_libk
+
+#define KERNEL_HEAP_START 0xC0500000
+#define KERNEL_HEAP_LENGTH 0x500000
+
+void	*kmalloc(size_t n_alloc);
+
+#elif defined __is_kernel
+
+#define KERNEL_HEAP_START 0xC0500000
+#define KERNEL_HEAP_LENGTH 0x500000
 
 void	*kmalloc(size_t n_alloc);
 
 #else
+
+#define HEAP_START 0x000000
+#define HEAP_LENGTH 0x500000
 
 void	*malloc(size_t n_alloc);
 
