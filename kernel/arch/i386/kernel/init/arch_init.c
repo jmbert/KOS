e7be834e1a3d9c7f610abe70aa9d7eb052e1aac2
jmbert <joe2157b@gmail.com>
2023-07-05 16:12:19 +0100

    Allocated space for the kernel heap in arch_init

--- kernel/arch/i386/kernel/init/arch_init.c
+++ kernel/arch/i386/kernel/init/arch_init.c
@@ -4,10 +4,14 @@
 #include <idt/idt.h>
 #include <tty.h>
 
+#include <pg/pframe.h>
 #include <pg/pg_map.h>
+#include <pg/pg_defs.h>
+#include <stdlib.h>
 
 void arch_init(multiboot_info_t *minfo) {
-    kmap_pages(GBUFFER, minfo->framebuffer_addr, (minfo->framebuffer_bpp * minfo->framebuffer_height * minfo->framebuffer_width)/0x1000, 0x3);
+    kmap_pages(GBUFFER, minfo->framebuffer_addr, (minfo->framebuffer_bpp * minfo->framebuffer_height * minfo->framebuffer_width)/PG_SIZE_SMALL, 0x3);
+    kmap_pages(KERNEL_HEAP_START, kalloc_pframe(), KERNEL_HEAP_LENGTH/PG_SIZE_SMALL, 0x3);
 
     gdt_init();
     idt_init();
