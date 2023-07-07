bca216e04e55148033375e88ff492ffa265dcb22
jmbert <joe2157b@gmail.com>
2023-07-07 21:07:32 +0100

    Finish commit

--- kernel/kernel/interrupts/isrs/init_isr.c
+++ kernel/kernel/interrupts/isrs/init_isr.c
@@ -1,6 +1,12 @@
 #include <kernel/interrupts/init_isr.h>
 
 static void register_exceptions(void) {
+
+    for (size_t vec = 0; vec < EXCEPTION_ISRS; vec++) {
+        register_isr(no_handler_enter, vec);
+    }
+    
+
     register_isr(division_error_enter,	DIVERR);
 	register_isr(debug_exception_enter,	DEBUG);
 	register_isr(invalid_opcode_enter,	INVOP);
