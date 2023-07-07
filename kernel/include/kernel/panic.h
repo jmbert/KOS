2dd651d7d41cecc4042bce68c2fa5ff7a80c4fbe
jmbert <joe2157b@gmail.com>
2023-07-07 18:55:16 +0100

    Added panic system

---
+++ kernel/include/kernel/panic.h
@@ -0,0 +1,22 @@
+#ifndef _PANIC_H
+#define _PANIC_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#if (MODE==0)
+#include <kernel/tty.h>
+
+#define PANIC_SCHEME 0x4f
+#define WARN_SCHEME 0xdf
+#define KERNEL_PANIC(ERR_FMT, ...)  tty_change_col(PANIC_SCHEME); \
+									printf(ERR_FMT, __VA_ARGS__); \
+									asm("cli;hlt"); \
+									for (;;);               /* So any noreturn functions will recognise a panic as noreturn */
+#define KERNEL_WARN(WARN_FMT, ...)  tty_change_col(WARN_SCHEME); \
+									printf(WARN_FMT, __VA_ARGS__);
+#else
+#error Graphics mode unimplemented
+#endif
+
+#endif
